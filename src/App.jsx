8b54dd650917dd33e543d10d704218d3450ff7d6
kavery-pm <shiny.kavery@gmail.com>
2023-05-23 22:01:50 +0200

    added the overflow when the number of players exceed viewport

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
  
 
   return (
-   <Box sx={{border:'1px solid white',  width: '300', height:'100vh', backgroundColor: '#5A4CDC', opacity: [0.9, 0.8, 0.7]
+   <Box sx={{border:'1px solid white',  width: '300', height:'100vh',overflowY:"auto", backgroundColor: '#5A4CDC', opacity: [0.9, 0.8, 0.7]
   }}>
     <h1>ScoreKeeper</h1>
     <ScoreKeeper/>
