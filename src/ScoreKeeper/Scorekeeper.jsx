750ccd1cc82dc51a5ce61b239383b8445c3e4473
kavery-pm <shiny.kavery@gmail.com>
2023-05-23 14:07:21 +0200

    added the generate scoreboard button

--- src/ScoreKeeper/Scorekeeper.jsx
+++ src/ScoreKeeper/Scorekeeper.jsx
@@ -1,11 +1,27 @@
 import { Box } from "@mui/material";
-import TextField from '@mui/material/TextField';
-
-const ScoreKeeper = ()=>{
-return (
-    <Box   component="form" sx={{border:'1px solid black',margin:'20px 20px', height:'100px',backgroundColor:'#CCE5FF',display:'flex',p:'20px'}}>
-<TextField id="outlined" label="Enter the number of players" variant="outlined"    sx={{ m: 1, width: '50ch',color:'black' }}/>
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+const ScoreKeeper = () => {
+  return (
+    <Box
+      component="form"
+      sx={{
+        border: "1px solid black",
+        margin: "20px 20px",
+        height: "100px",
+        backgroundColor: "#CCE5FF",
+        
+        p: "20px",
+      }}
+    >
+      <TextField
+        id="outlined"
+        label="Enter the number of players"
+        variant="outlined"
+        sx={{ width: "50ch", color: "black",m:'10px',justifyContent:'center' }}
+      />
+      <Button variant="contained">Generate Score Board</Button>
     </Box>
-)
-}
+  );
+};
 export default ScoreKeeper;
