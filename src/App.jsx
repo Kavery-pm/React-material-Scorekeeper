626958dfeafa13709377a50d0a2763530b8746cd
kavery-pm <shiny.kavery@gmail.com>
2023-05-23 22:16:02 +0200

    added the score statically

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
-import Box from '@mui/material/Box';
-import './App.css'
-import ScoreKeeper from './ScoreKeeper/Scorekeeper';
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import "./App.css";
+import ScoreKeeper from "./ScoreKeeper/Scorekeeper";
 
 function App() {
- 
-
   return (
-   <Box sx={{border:'1px solid white',  width: '300', height:'100vh',overflowY:"auto", backgroundColor: '#5A4CDC', opacity: [0.9, 0.8, 0.7]
-  }}>
-    <h1>ScoreKeeper</h1>
-    <ScoreKeeper/>
-   </Box>
-  )
+    <Box
+      sx={{
+        border: "1px solid white",
+        width: "300",
+        height: "100vh",
+        overflowY: "auto",
+        backgroundColor: "#5A4CDC",
+        opacity: [0.9, 0.8, 0.7],
+      }}
+    >
+      <h1>ScoreKeeper</h1>
+      <ScoreKeeper />
+    </Box>
+  );
 }
 
-export default App
+export default App;
--- src/ScoreBoard/Scoreboard.jsx
+++ src/ScoreBoard/Scoreboard.jsx
@@ -1,32 +1,46 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
 import { borderRadius } from "@mui/system";
 // import { deepOrange} from '@mui/material/colors';
-const Scoreboard = ({playerNumbers})=>{
-    console.log(typeof playerNumbers)
-    const [scoreBoard, setscoreBoard] = useState(new Array(+playerNumbers).fill(0));
-   
-    console.log(new Array(+playerNumbers).fill(0))
-return (
+const Scoreboard = ({ playerNumbers }) => {
+  console.log(typeof playerNumbers);
+  const [scoreBoard, setscoreBoard] = useState(
+    new Array(+playerNumbers).fill(0)
+  );
+
+  console.log(new Array(+playerNumbers).fill(0));
+  return (
     <>
-    {scoreBoard.map((item,idx)=>(
-
-    <Stack direction="horizontal" spacing={1} sx={{marginTop:'30px'}} key={idx}>
-    <Chip label={`player${idx}`} size="small"sx={{width:'100px',height:'50px'}} color="success" />
-    <Button variant="contained" color="success"sx={{width:'70px',borderRadius:'20px'}} >
-+1
-</Button>
-    </Stack>
-)
-
-)
-}
-</> 
-)
-
-}
+      {scoreBoard.map((item, idx) => (
+        <Stack
+          direction="horizontal"
+          spacing={1}
+          sx={{ marginTop: "30px", marginLeft: "20px" }}
+          key={idx}
+        >
+          <div style={{marginLeft:'80px'}}>
+            <Chip
+              label={`player${idx}`}
+              size="small"
+              sx={{ width: "100px", height: "50px",marginRight:'20px' }}
+              color="success"
+            />
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ width: "70px", borderRadius: "20px",marginRight:'20px'  }}
+            >
+              +1
+            </Button>
+            <label>Score:<span style={{fontSize:'20px'}}> &nbsp;{item}</span></label>
+          </div>
+        </Stack>
+      ))}
+    </>
+  );
+};
 export default Scoreboard;
