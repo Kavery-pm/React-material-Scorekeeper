0410ae273b8dc9111fdae4ce570dbb69862b01e3
kavery-pm <shiny.kavery@gmail.com>
2023-05-24 11:22:30 +0200

    added the modal to display the winner when the winner score is met

--- src/ScoreBoard/Scoreboard.jsx
+++ src/ScoreBoard/Scoreboard.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { blue } from '@mui/material/colors';
 // import { deepOrange} from '@mui/material/colors';
 const Scoreboard = ({ playerNumbers }) => {
+  const [open, setOpen] = useState(true);
+  const winningScore = useRef(5);
+ const [winScore, setwinScore] = useState(10);
   console.log(typeof playerNumbers);
   const [scoreBoard, setscoreBoard] = useState(
     new Array(+playerNumbers).fill(0)
@@ -17,10 +27,22 @@ const Scoreboard = ({ playerNumbers }) => {
       });
     });
   };
-  
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
+      <TextField
+        id="outlined"
+        label="Enter winning Score"
+        variant="filled"
+        value={winScore}
+     onChange={(event)=>{setwinScore(event.target.value)}}
+        sx={{ width: "50ch", color: "white",m:'20px',justifyContent:'center' }}
+      />
+      
+ <p>Winning score{winScore}</p>
       {scoreBoard.map((item, idx) => (
         <Stack
           direction="horizontal"
@@ -29,8 +51,9 @@ const Scoreboard = ({ playerNumbers }) => {
           key={idx}
         >
           <div style={{ marginLeft: "80px" }}>
+           
             <Chip
-              label={`player${idx}`}
+              label={`player${idx + 1}`}
               size="small"
               sx={{ width: "100px", height: "50px", marginRight: "20px",backgroundColor:'#000080',color:'white' }}
              
@@ -46,6 +69,30 @@ const Scoreboard = ({ playerNumbers }) => {
             <label>
               Score:<span style={{ fontSize: "20px" }}> &nbsp;{item}</span>
             </label>
+            {item>=winScore && item!==0 &&
+              <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+              sx={{color:'white'}}
+            >
+              <DialogTitle id="alert-dialog-title">
+              CONGRATS
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                 `player {idx+1} is the winner`
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+              
+                <Button onClick={handleClose} autoFocus>
+                 OK
+                </Button>
+              </DialogActions>
+            </Dialog>
+            }
           </div>
         </Stack>
       ))}
