a47a5fd0e5a8d6270293450c55833491e5e6cdef
kavery-pm <shiny.kavery@gmail.com>
2023-05-24 07:10:23 +0200

    added the increment score function for a player without affecting the scores of other players

--- src/ScoreBoard/Scoreboard.jsx
+++ src/ScoreBoard/Scoreboard.jsx
@@ -2,16 +2,21 @@
 import { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import { borderRadius } from "@mui/system";
 // import { deepOrange} from '@mui/material/colors';
 const Scoreboard = ({ playerNumbers }) => {
   console.log(typeof playerNumbers);
   const [scoreBoard, setscoreBoard] = useState(
     new Array(+playerNumbers).fill(0)
   );
-
+  const incrementScoreHandler = (idx) => {
+    setscoreBoard((previousScore) => {
+      return previousScore.map((score, index) => {
+        if (index === idx) return score + 1;
+        else return score;
+      });
+    });
+  };
   console.log(new Array(+playerNumbers).fill(0));
   return (
     <>
@@ -22,21 +27,24 @@ const Scoreboard = ({ playerNumbers }) => {
           sx={{ marginTop: "30px", marginLeft: "20px" }}
           key={idx}
         >
-          <div style={{marginLeft:'80px'}}>
+          <div style={{ marginLeft: "80px" }}>
             <Chip
               label={`player${idx}`}
               size="small"
-              sx={{ width: "100px", height: "50px",marginRight:'20px' }}
-              color="success"
+              sx={{ width: "100px", height: "50px", marginRight: "20px",backgroundColor:'#000080',color:'white' }}
+             
             />
             <Button
               variant="contained"
-              color="success"
-              sx={{ width: "70px", borderRadius: "20px",marginRight:'20px'  }}
+             
+              sx={{ width: "70px", borderRadius: "20px", marginRight: "20px" ,backgroundColor:'#000080'}}
+              onClick={incrementScoreHandler.bind(this, idx)}
             >
               +1
             </Button>
-            <label>Score:<span style={{fontSize:'20px'}}> &nbsp;{item}</span></label>
+            <label>
+              Score:<span style={{ fontSize: "20px" }}> &nbsp;{item}</span>
+            </label>
           </div>
         </Stack>
       ))}
